e731f03ba343df95b92e2460c963e9db1fc072ef
sudesh.tnbt <sudeshacharya110@gmail.com>
2024-07-14 22:39:48 +0545

    Updated the randomization, added list to match the guess to the letters.

--- hangman.py
+++ hangman.py
@@ -4,14 +4,13 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-index = random.randint(0, 2)
-word = word_list[index]
+word = random.choice(word_list)
+display = ["_"] * len(word)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ")
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 for i in range(len(word)):
     if guess == word[i]:
-        print("Right")
-    else:
-        print("Wrong")
+        display[i] = guess
+print(display)
 
